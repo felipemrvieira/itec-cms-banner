7783df1ad722caeb8bfb16e98228f1ec80ecb445
Felipe Maciel <felipemrvieira@gmail.com>
2019-04-15 11:58:07 -0300

    Cria componente banner

--- Banner.js
+++ Banner.js
@@ -1,1 +1,20 @@
-banner
+import React, { Component } from 'react';
+import './Banner.scss';
+
+
+class Banner extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="banner-container">
+                <img src={this.props.src} alt={this.props.alt} />
+            </div>
+        );
+    }
+}
+
+export default Banner;
+
+
